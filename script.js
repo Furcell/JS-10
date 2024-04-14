7eba6d44e761fa63eacb0b621f3f3f039834c626
Lashapurtseladze <149567152+Furcell@users.noreply.github.com>
2024-04-14 17:53:21 +0400

    finish JS-10-luka

--- script.js
+++ script.js
@@ -3,50 +3,49 @@
 // HomeWork-10
 let input = document.getElementById("input-filter");
 let ulElement = document.getElementById("result");
-let listItem = [];
+let listItems = [];
+const API_URL = "https://jsonplaceholder.typicode.com/users";
 
 
-async function asyncFnc() {
-    try {
-        let response = await fetch("https://jsonplaceholder.typicode.com/users");
-        console.log(response);
-        if (!response.ok) {
-            throw new Error("Can Not Fetch Data");
-        }
-        let responseData = await response.json();
 
-        responseData.forEach(el => {
-            let li = document.createElement("li");
-            li.innerText = `${el.email}`;
-
-            listItem.push(li);
-            console.log(listItem);
-            ulElement.appendChild(li);
-        });
-
-    } catch (error) {
-        console.log(error);
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    console.log(response);
+    if (!!response) {
+      return await response.json();
     }
+  } catch (error) {
+    throw new Error("Can Not Fetch Data");
+  }
 }
-asyncFnc();
 
+async function createListItem() {
+  const responseData = await fetchData(API_URL);
+  if (Array.isArray(responseData) && responseData.length != 0) {
+    responseData.forEach((el) => {
+      let li = document.createElement("li");
+      li.innerText = `${el.email}`;
 
-
-function filterData(searchItem) {        
-    listItem.forEach((item) => {
-        if (item.innerText.toLowerCase().includes(searchItem.toLowerCase())) { 
-            item.classList.remove("active");
-        } else {
-            item.classList.add("active");
-        }
+      listItems.push(li);
+      ulElement.appendChild(li);
     });
+  }
+}
+createListItem();
+
+
+
+function filterData(searchItem) {
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchItem.toLowerCase())) {
+      item.classList.remove("active");
+    } else {
+      item.classList.add("active");
+    }
+  });
 }
 
-
-input.addEventListener("keyup",function () {
-    filterData(this.value);
-})
-
-
-
-
+input.addEventListener("keyup", function () {
+  filterData(this.value);
+});
